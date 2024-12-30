abd1414b8f5f99c67e93bc241d294d93da332a9b
Lucas Augusto <ldlaconcursos@gmail.com>
2024-12-30 17:45:43 -0300

    reboot

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const sequelize = require('./config/database');
-const PORT = process.env.PORT || 3000;
-
-sequelize.sync({ alter: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-  });
-});
